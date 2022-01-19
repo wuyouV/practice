5ce2ec86c071f03170b1b539d2070323306fa4d4
1494456548@qq.com <1494456548@qq.com>
2022-01-19 14:14:53 +0800

    update DevA&DevB

--- src/main/java/com/hwp/test/git/DevA.java
+++ src/main/java/com/hwp/test/git/DevA.java
@@ -3,6 +3,7 @@ package com.hwp.test.git;
 public class DevA {
 
     public static void main(String[] args) {
-
+        String hello = "hello! coder.";
+        System.out.println("hello");
     }
 }
