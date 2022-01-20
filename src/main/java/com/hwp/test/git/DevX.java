6d69e04b6e63945943fed001d94879c1572146ee
1494456548@qq.com <1494456548@qq.com>
2022-01-20 10:12:21 +0800

    int a,b,c,d;

--- src/main/java/com/hwp/test/git/DevX.java
+++ src/main/java/com/hwp/test/git/DevX.java
@@ -3,6 +3,9 @@ package com.hwp.test.git;
 public class DevX {
 
     public static void main(String[] args) {
-
+        int a = 1;
+        int b = 1;
+        int c = 2;
+        int d = 10;
     }
 }
