99d237b91cfab44a516539355f017d6602f458f8
1494456548@qq.com <1494456548@qq.com>
2022-01-26 13:21:48 +0800

    long a&b&c;

--- src/main/java/com/hwp/test/git/DevY.java
+++ src/main/java/com/hwp/test/git/DevY.java
@@ -3,6 +3,8 @@ package com.hwp.test.git;
 public class DevY {
 
     public static void main(String[] args) {
-        
+        long a = 1L;
+        long b = 10L;
+        long c = 100L;
     }
 }
